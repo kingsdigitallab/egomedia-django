224cb3e573b6f7fc3388c19bd339e43d8721128c
Miguel Vieira <jmvieira@gmail.com>
2019-02-06 16:16:42 +0000

    Set up page parent types and subtypes

--- core/blocks.py
+++ core/blocks.py
@@ -4,10 +4,17 @@ from wagtail.core.blocks import (CharBlock, ListBlock, RichTextBlock,
                                  StreamBlock, StructBlock, TextBlock)
 
 
+# TODO add block similar to timeline block, the options are text (richtext)
+# and one of document, image, link, embed, table
+
 class TimelineBlock(StructBlock):
+    # TODO rename this to node title
     title = CharBlock()
+    # TODO remove this field
     label = CharBlock()
+    # TODO add a tag field, but wait for the meeting for feedback
     item = StreamBlock([
+        # TODO convert description to rich text
         ('description', TextBlock(required=False)),
         ('document_block', DocumentBlock(required=False)),
         ('image_block', ImageBlock(required=False)),
@@ -30,4 +37,6 @@ class HomePageStreamBlock(StreamBlock):
 
 
 class TimelineStreamBlock(BaseStreamBlock):
+    # TODO rename this sequence instead of timeline
+    # TODO add a title field
     timeline_block = ListBlock(TimelineBlock(), icon='list-ol')
--- core/models.py
+++ core/models.py
@@ -1,12 +1,14 @@
 from core.blocks import HomePageStreamBlock, TimelineStreamBlock
+from django.db import models
+from kdl_wagtail.core.models import (BasePage, BaseStreamPage, IndexPage,
+                                     RichTextPage, StreamPage)
+from kdl_wagtail.people.models import (PeopleIndexPage, Person, PersonModel,
+                                       PersonPage)
 from wagtail.admin.edit_handlers import StreamFieldPanel
+from wagtail.api import APIField
 from wagtail.core.fields import StreamField
 from wagtail.core.models import Page
 from wagtail.search import index
-from kdl_wagtail.people.models import Person, PersonModel
-from kdl_wagtail.core.models import BasePage, BaseStreamPage
-from django.db import models
-from wagtail.api import APIField
 from wagtail.snippets.edit_handlers import SnippetChooserPanel
 
 
@@ -21,8 +23,12 @@ class HomePage(Page):
         index.SearchField('body')
     ]
 
+    subpage_types = [IndexPage, PeopleIndexPage, StreamPage]
+
 
 class ResearcherPage(BaseStreamPage):
+    # TODO add links to facets
+    # TODO add link to themes
     person = models.ForeignKey(
         PersonModel, related_name='researcher_pages', on_delete=models.PROTECT)
 
@@ -37,6 +43,9 @@ class ResearcherPage(BaseStreamPage):
     search_fields = BaseStreamPage.search_fields + [
         index.RelatedFields('person', Person.search_fields)
     ]
+
+    parent_page_types = [PeopleIndexPage]
+    subpage_types = []
 
 
 class BaseTimelinePage(BasePage):
@@ -60,8 +69,35 @@ class BaseTimelinePage(BasePage):
 
 
 class ProjectPage(BaseTimelinePage):
-    pass
+    # TODO add links to facets
+    # TODO add link to person
+
+    parent_page_types = [IndexPage, 'ProjectPage']
+    subpage_types = ['ProjectPage']
 
 
 class ThemePage(BaseTimelinePage):
-    pass
+    # TODO add links to facets
+    # TODO add link to themes
+
+    parent_page_types = [IndexPage]
+    subpage_types = []
+
+
+# TODO add snippets for keyword, focus, method, discipline
+
+# Sets up pages' visibility
+IndexPage.parent_page_types = [HomePage, IndexPage]
+IndexPage.subpage_types = [IndexPage, ProjectPage, StreamPage, ThemePage]
+PeopleIndexPage.parent_page_types = [HomePage]
+PeopleIndexPage.subpage_types = [ResearcherPage]
+PersonPage.parent_page_types = []
+PersonPage.subpage_types = []
+ProjectPage.subpage_types = [ProjectPage]
+RichTextPage.parent_page_types = []
+RichTextPage.subpage_types = []
+StreamPage.parent_page_types = [HomePage, IndexPage]
+
+# Re-label the stream page to match the name required by this project
+StreamPage._meta.verbose_name = 'Generic page'
+StreamPage._meta.verbose_name_plural = 'Generic pages'
