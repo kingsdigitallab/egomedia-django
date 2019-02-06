5c63ebe9b8dc544a997b733a821172085655eb98
Miguel Vieira <jmvieira@gmail.com>
2019-02-06 18:12:28 +0000

    Converted timeline item description to richtext, added timeline item block and heading.

--- core/blocks.py
+++ core/blocks.py
@@ -1,26 +1,31 @@
 from kdl_wagtail.core.blocks import (BaseStreamBlock, DocumentBlock,
                                      EmbedBlock, ImageBlock, LinkBlock)
 from wagtail.core.blocks import (CharBlock, ListBlock, RichTextBlock,
-                                 StreamBlock, StructBlock, TextBlock)
+                                 StreamBlock, StructBlock)
 
 
 # TODO add block similar to timeline block, the options are text (richtext)
 # and one of document, image, link, embed, table
 
-class TimelineBlock(StructBlock):
-    # TODO rename this to node title
+class TimelineItemBlock(StructBlock):
     title = CharBlock()
-    # TODO remove this field
-    label = CharBlock()
-    # TODO add a tag field, but wait for the meeting for feedback
+    # TODO possibly add a tag field, but wait for the meeting for feedback
     item = StreamBlock([
-        # TODO convert description to rich text
-        ('description', TextBlock(required=False)),
+        ('description', RichTextBlock(required=False)),
         ('document_block', DocumentBlock(required=False)),
         ('image_block', ImageBlock(required=False)),
         ('link_block', LinkBlock(required=False)),
         ('embed_block', EmbedBlock(required=False))
     ])
+
+    class Meta:
+        icon = 'list-ol'
+        template = 'core/blocks/timeline_item_block.html'
+
+
+class TimelineBlock(StructBlock):
+    title = CharBlock()
+    items = ListBlock(TimelineItemBlock())
 
     class Meta:
         icon = 'list-ol'
@@ -30,7 +35,7 @@ class TimelineBlock(StructBlock):
 class HomePageStreamBlock(StreamBlock):
     richtext_block = RichTextBlock(
         icon='pilcrow',
-        template='kdl_wagtail/blocks/richtext_block.html'
+        template='kdl_wagtail_core/blocks/richtext_block.html'
     )
     link_block = LinkBlock(required=False)
     timeline_block = ListBlock(TimelineBlock(), icon='list-ol')
@@ -39,4 +44,4 @@ class HomePageStreamBlock(StreamBlock):
 class TimelineStreamBlock(BaseStreamBlock):
     # TODO rename this sequence instead of timeline
     # TODO add a title field
-    timeline_block = ListBlock(TimelineBlock(), icon='list-ol')
+    timeline_block = TimelineBlock()
