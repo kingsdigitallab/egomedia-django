2120cfacd370edf7852f03a1bf5c9a0fd9662245
ginestra <ginestra@users.noreply.github.com>
2019-03-05 11:53:50 +0000

    Pipfile.lock updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,13 @@
-
-$(document).foundation();
-
 $(document).ready(function() {
 
+    $(document).foundation();
+
+    // Table of Contents
+    // TODO: Improve to use pure CSS selectors as jQuery recommends here:
+    // https://api.jquery.com/header-selector/
+
+    $( ":header" ).each( function() {
+        alert("this is a heading");
+    });
 
 });
