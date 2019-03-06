5db7d1334d60f7c610aa32496affa3640b46f1d2
ginestra <ginestra@users.noreply.github.com>
2019-03-06 10:03:22 +0000

    Added js-cookie vendor, added cookie disclaimer

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,4 +9,22 @@ $(document).ready(function() {
     // $( ":header" ).each( function() {
     //     alert("this is a heading");
     // });
+
+    // Expande / Collapse
+    $('.toggler').on('click', function () {
+        $(this).next('.content').slideToggle(400).toggleClass('hide show');
+        $(this).toggleClass('close open');
+
+        return false;
+    });
+
+    // Cookie disclaimer
+    if (!Cookies.get('ego-cookie')) {
+        $("#cookie-disclaimer").removeClass('hide');
+    }
+    // Set cookie and hide the box
+    $('#cookie-disclaimer .close').on("click", function() {
+        Cookies.set('ego-cookie', 'ego-cookie-set', { expires: 30 });
+        $("#cookie-disclaimer").addClass('hide');
+    });
 });
