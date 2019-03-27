4dbb8fd3c200abfb74851de198ba6cd46e8a0b67
Miguel Vieira <jmvieira@gmail.com>
2019-03-27 18:01:27 +0000

    Refactor to make the code more readable.

--- assets/js/filters.js
+++ assets/js/filters.js
@@ -1,9 +1,5 @@
 $(document).ready(function() {
-  const $results = $('#results').isotope({
-    itemSelector: '.cell',
-    layoutMode: 'fitRows'
-  });
-
+  // add the cards' tags as classes in the cells for a cleaner isotope filtering
   $('#results .cell').each(function(i, cell) {
     const tags = cell.querySelectorAll('.tag');
 
@@ -14,27 +10,53 @@ $(document).ready(function() {
 
   var filters = {};
 
+  const $results = $('#results').isotope({
+    itemSelector: '.cell',
+    layoutMode: 'fitRows'
+  });
+
+  // apply filters
   $('.filter').change(function() {
     const category = $(this).data('category');
     const value = $(this).val();
 
     filters[category] = value;
 
-    const filterValue = Object.values(filters).join('').trim();
+    const filterValue = getFilterValue(filters);
 
+    filterResults($results, filterValue);
+
+    toggleClearFilters(filterValue);
+
+    disableUnavailableFilters($results);
+  });
+
+  function getFilterValue(filters) {
+    if (!filters) {
+      return '';
+    }
+
+    return Object.values(filters).join('').trim();
+  }
+
+  function filterResults(isotopeObj, filterValue) {
+    isotopeObj.isotope({
+      filter: filterValue
+    });
+  }
+
+  function toggleClearFilters(filterValue) {
     if (filterValue.length > 0) {
       $('#clear-filters').removeAttr('disabled');
     } else {
       $('#clear-filters').attr('disabled', true);
     }
+  }
 
-    $results.isotope({
-      filter: filterValue
-    });
-
-    const items = $results.isotope('getFilteredItemElements');
+  function disableUnavailableFilters(isotopeObj) {
+    const filteredItems = isotopeObj.isotope('getFilteredItemElements');
     const availableFilters = [
-      ...new Set(items.flatMap(x => x.className.split(' ')))
+      ...new Set(filteredItems.flatMap(x => x.className.split(' ')))
     ];
 
     $('.filter option').each(function(i, option) {
@@ -48,21 +70,27 @@ $(document).ready(function() {
         }
       }
     });
+  }
+
+  // remove filters
+  $('#clear-filters').click(function() {
+    filters = {};
+
+    const filterValue = getFilterValue(filters);
+
+    filterResults($results, filterValue);
+
+    toggleClearFilters(filterValue);
+
+    resetFilters();
   });
 
-  $('#clear-filters').click(function() {
-    $(this).attr('disabled', true);
-
-    filters = {};
-    $results.isotope({
-      filter: '*'
-    });
-
-    $('.filter option:disabled').each(function(i, option) {
-      $(option).removeAttr('disabled');
-    });
+  function resetFilters() {
     $('.filter').each(function(i, select) {
       select.selectedIndex = 0;
     });
-  });
+    $('.filter option:disabled').each(function(i, option) {
+      $(option).removeAttr('disabled');
+    });
+  }
 });
