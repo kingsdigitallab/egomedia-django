2b32f2b04c6036af8b4d84bf2f55713c812385d3
Miguel Vieira <jmvieira@gmail.com>
2019-03-27 15:20:20 +0000

    Add button to clear all filters.

--- assets/js/filters.js
+++ assets/js/filters.js
@@ -1,44 +1,62 @@
 $(document).ready(function() {
-  const $results = $("#results").isotope({
-    itemSelector: ".cell",
-    layoutMode: "fitRows"
+  const $results = $('#results').isotope({
+    itemSelector: '.cell',
+    layoutMode: 'fitRows'
   });
 
-  $("#results .cell").each(function(i, cell) {
-    const tags = cell.querySelectorAll(".tag");
+  $('#results .cell').each(function(i, cell) {
+    const tags = cell.querySelectorAll('.tag');
 
     $(tags).each(function(i, tag) {
-      $(cell).addClass($(tag).data("filter"));
+      $(cell).addClass($(tag).data('filter'));
     });
   });
 
   var filters = {};
 
-  $(".filter").change(function() {
-    const category = $(this).data("category");
+  $('.filter').change(function() {
+    const category = $(this).data('category');
     const value = $(this).val();
 
     filters[category] = value;
 
-    const filterValue = Object.values(filters).join("");
+    const filterValue = Object.values(filters).join('');
+
+    if (filterValue.length > 0) {
+      $('#clear-filters').removeAttr('disabled');
+    }
 
     $results.isotope({ filter: filterValue });
 
-    const items = $results.isotope("getFilteredItemElements");
+    const items = $results.isotope('getFilteredItemElements');
     const availableFilters = [
-      ...new Set(items.flatMap(x => x.className.split(" ")))
+      ...new Set(items.flatMap(x => x.className.split(' ')))
     ];
 
-    $(".filter option").each(function(i, option) {
-      const value = option.value.replace(/\./, "");
+    $('.filter option').each(function(i, option) {
+      const value = option.value.replace(/\./, '');
 
       if (value) {
         if (availableFilters.includes(value)) {
-          $(option).removeAttr("disabled");
+          $(option).removeAttr('disabled');
         } else {
-          $(option).attr("disabled", true);
+          $(option).attr('disabled', true);
         }
       }
+    });
+  });
+
+  $('#clear-filters').click(function() {
+    $(this).attr('disabled', true);
+
+    filters = {};
+    $results.isotope({ filter: '*' });
+
+    $('.filter option:disabled').each(function(i, option) {
+      $(option).removeAttr('disabled');
+    });
+    $('.filter').each(function(i, select) {
+      select.selectedIndex = 0;
     });
   });
 });
