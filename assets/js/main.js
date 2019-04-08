d060730dd046fedd24b80f2629732ae30fe9cf89
Miguel Vieira <jmvieira@gmail.com>
2019-04-08 14:06:15 +0100

    Add external class and target attribute to external links.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,32 +1,33 @@
 $(document).ready(function() {
-  $(document).foundation();
+  $(document).foundation()
 
-  // Table of Contents
-  // TODO: Improve to use pure CSS selectors as jQuery recommends here:
-  // https://api.jquery.com/header-selector/
-
-  // $( ":header" ).each( function() {
-  //     alert("this is a heading");
-  // });
+  $('a')
+    .filter(function() {
+      return this.hostname && this.hostname !== location.hostname
+    })
+    .addClass('external')
+    .attr('target', '_blank')
 
   // Expande / Collapse
-  $(".toggler").on("click", function() {
+  $('.toggler').on('click', function() {
     $(this)
-      .siblings(".sub")
+      .siblings('.sub')
       .slideToggle(400)
-      .toggleClass("hide show");
-    $(this).toggleClass("close open");
+      .toggleClass('hide show')
+    $(this).toggleClass('close open')
 
-    return false;
-  });
+    return false
+  })
 
   // Cookie disclaimer
-  if (!Cookies.get("ego-cookie")) {
-    $("#cookie-disclaimer").removeClass("hide");
+  if (!Cookies.get('ego-cookie')) {
+    $('#cookie-disclaimer').removeClass('hide')
   }
   // Set cookie and hide the box
-  $("#cookie-disclaimer .close").on("click", function() {
-    Cookies.set("ego-cookie", "ego-cookie-set", { expires: 30 });
-    $("#cookie-disclaimer").addClass("hide");
-  });
-});
+  $('#cookie-disclaimer .close').on('click', function() {
+    Cookies.set('ego-cookie', 'ego-cookie-set', {
+      expires: 30
+    })
+    $('#cookie-disclaimer').addClass('hide')
+  })
+})
