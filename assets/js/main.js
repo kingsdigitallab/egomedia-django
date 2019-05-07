9445626cbaec0d78803226908c65c954ffaaa344
Miguel Vieira <jmvieira@gmail.com>
2019-05-07 15:37:39 +0100

    Add function to copy anchor URLs.

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,6 +8,28 @@ $(document).ready(function() {
     .addClass('external')
     .attr('target', '_blank')
 
+  $('.anchor-link').click(function() {
+    try {
+      let $field = $('<input>', {
+        'class': 'to-copy',
+        'type': 'text',
+        'value': location.href.replace(location.hash, '') + '#' + $(
+            this)
+          .attr('id')
+      })
+
+      $(this).append($field)
+      $field.focus()
+      $field.select()
+
+      document.execCommand('copy')
+
+      $field.remove()
+    } catch (err) {
+      console.log('copy execCommand failed', err)
+    }
+  })
+
   // Expande / Collapse
   $('.toggler').on('click', function() {
     $(this)
