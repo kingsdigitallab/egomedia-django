c5dfbded20002bdfb8c35b42d7fc3684bfdae7cf
Miguel Vieira <jmvieira@pm.me>
2022-03-18 10:55:22 +0000

    🐛 Fix: Youtube embeddings.

--- egomedia/settings/base.py
+++ egomedia/settings/base.py
@@ -18,8 +18,8 @@ from kdl_ldap.settings import *  # noqa
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 
-PROJECT_NAME = 'egomedia'
-PROJECT_TITLE = 'Ego Media'
+PROJECT_NAME = "egomedia"
+PROJECT_TITLE = "Ego Media"
 
 # -----------------------------------------------------------------------------
 # Core Settings
@@ -35,16 +35,16 @@ ALLOWED_HOSTS = []
 # https://docs.djangoproject.com/en/dev/topics/cache/
 # http://redis.io/topics/lru-cache
 # http://niwibe.github.io/django-redis/
-CACHE_REDIS_DATABASE = '0'
+CACHE_REDIS_DATABASE = "0"
 
 CACHES = {
-    'default': {
-        'BACKEND': 'django_redis.cache.RedisCache',
-        'LOCATION': 'redis://127.0.0.1:6379/' + CACHE_REDIS_DATABASE,
-        'OPTIONS': {
-            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
-            'IGNORE_EXCEPTIONS': True
-        }
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/" + CACHE_REDIS_DATABASE,
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+            "IGNORE_EXCEPTIONS": True,
+        },
     }
 }
 
@@ -57,184 +57,169 @@ DEBUG = False
 # EMAIL SETTINGS
 # -----------------------------------------------------------------------------
 
-DEFAULT_FROM_EMAIL = 'noreply@kcl.ac.uk'
-EMAIL_HOST = 'smtp.cch.kcl.ac.uk'
+DEFAULT_FROM_EMAIL = "noreply@kcl.ac.uk"
+EMAIL_HOST = "smtp.cch.kcl.ac.uk"
 EMAIL_PORT = 25
-EMAIL_HOST_USER = ''
-EMAIL_HOST_PASSWORD = ''
-EMAIL_SUBJECT_PREFIX = '[Django {}] '.format(PROJECT_NAME)
+EMAIL_HOST_USER = ""
+EMAIL_HOST_PASSWORD = ""
+EMAIL_SUBJECT_PREFIX = "[Django {}] ".format(PROJECT_NAME)
 EMAIL_USE_TLS = False
 
 # Sender of error messages to ADMINS and MANAGERS
 SERVER_EMAIL = DEFAULT_FROM_EMAIL
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.humanize',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.sitemaps',
-    'django.contrib.staticfiles',
-
-    'activecollab_digger',
-    'compressor',
-    'django_extensions',
-
-    'kdl_ldap',
-    'rest_framework',
-
-    'wagtail.contrib.forms',
-    'wagtail.contrib.modeladmin',
-    'wagtail.contrib.redirects',
-    'wagtail.contrib.routable_page',
-    'wagtail.contrib.settings',
-    'wagtail.contrib.styleguide',
-    'wagtail.contrib.table_block',
-    'wagtail.embeds',
-    'wagtail.sites',
-    'wagtail.users',
-    'wagtail.snippets',
-    'wagtail.documents',
-    'wagtail.images',
-    'wagtail.search',
-    'wagtail.admin',
-    'wagtail.core',
-
-    'modelcluster',
-    'taggit',
-    'wagtailmenus',
-
-    'bakery',
-    'wagtailbakery'
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.humanize",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.sitemaps",
+    "django.contrib.staticfiles",
+    "activecollab_digger",
+    "compressor",
+    "django_extensions",
+    "kdl_ldap",
+    "rest_framework",
+    "wagtail.contrib.forms",
+    "wagtail.contrib.modeladmin",
+    "wagtail.contrib.redirects",
+    "wagtail.contrib.routable_page",
+    "wagtail.contrib.settings",
+    "wagtail.contrib.styleguide",
+    "wagtail.contrib.table_block",
+    "wagtail.embeds",
+    "wagtail.sites",
+    "wagtail.users",
+    "wagtail.snippets",
+    "wagtail.documents",
+    "wagtail.images",
+    "wagtail.search",
+    "wagtail.admin",
+    "wagtail.core",
+    "modelcluster",
+    "taggit",
+    "wagtailmenus",
+    "bakery",
+    "wagtailbakery",
 ]
 
 INSTALLED_APPS += [
-    'kdl_wagtail.core',
-    'kdl_wagtail.people',
-    'kdl_wagtail.zotero',
-    'core.apps.CoreConfig'
+    "kdl_wagtail.core",
+    "kdl_wagtail.people",
+    "kdl_wagtail.zotero",
+    "core.apps.CoreConfig",
 ]
 
-INTERNAL_IPS = ['127.0.0.1']
+INTERNAL_IPS = ["127.0.0.1"]
 
 # https://docs.djangoproject.com/en/dev/topics/i18n/
-LANGUAGE_CODE = 'en-gb'
-TIME_ZONE = 'Europe/London'
+LANGUAGE_CODE = "en-gb"
+TIME_ZONE = "Europe/London"
 USE_I18N = True
 USE_L10N = False
 USE_TZ = True
 
-LOGGING_ROOT = os.path.join(BASE_DIR, 'logs')
+LOGGING_ROOT = os.path.join(BASE_DIR, "logs")
 LOGGING_LEVEL = logging.WARN
 
 if not os.path.exists(LOGGING_ROOT):
     os.makedirs(LOGGING_ROOT)
 
 LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'formatters': {
-        'verbose': {
-            'format': ('%(levelname)s %(asctime)s %(module)s '
-                       '%(process)d %(thread)d %(message)s')
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "verbose": {
+            "format": (
+                "%(levelname)s %(asctime)s %(module)s "
+                "%(process)d %(thread)d %(message)s"
+            )
         },
-        'simple': {
-            'format': '%(levelname)s %(message)s'
-        }
+        "simple": {"format": "%(levelname)s %(message)s"},
     },
-    'handlers': {
-        'file': {
-            'level': 'DEBUG',
-            'class': 'logging.handlers.TimedRotatingFileHandler',
-            'filename': os.path.join(LOGGING_ROOT, 'django.log'),
-            'formatter': 'verbose'
+    "handlers": {
+        "file": {
+            "level": "DEBUG",
+            "class": "logging.handlers.TimedRotatingFileHandler",
+            "filename": os.path.join(LOGGING_ROOT, "django.log"),
+            "formatter": "verbose",
         },
-        'console': {
-            'level': 'DEBUG',
-            'class': 'logging.StreamHandler',
-            'formatter': 'simple'
+        "console": {
+            "level": "DEBUG",
+            "class": "logging.StreamHandler",
+            "formatter": "simple",
         },
-        'mail_admins': {
-            'level': 'ERROR',
-            'class': 'django.utils.log.AdminEmailHandler'
-        }
+        "mail_admins": {
+            "level": "ERROR",
+            "class": "django.utils.log.AdminEmailHandler",
+        },
     },
-    'loggers': {
-        'django': {
-            'handlers': ['file', 'mail_admins'],
-            'level': LOGGING_LEVEL,
-            'propagate': True
+    "loggers": {
+        "django": {
+            "handlers": ["file", "mail_admins"],
+            "level": LOGGING_LEVEL,
+            "propagate": True,
         },
-        'egomedia': {
-            'handlers': ['file'],
-            'level': LOGGING_LEVEL,
-            'propagate': True
-        },
-    }
+        "egomedia": {"handlers": ["file"], "level": LOGGING_LEVEL, "propagate": True},
+    },
 }
 
 MIDDLEWARE = [
-    'django.middleware.gzip.GZipMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.security.SecurityMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-
-    'wagtail.core.middleware.SiteMiddleware',
-    'wagtail.contrib.redirects.middleware.RedirectMiddleware',
+    "django.middleware.gzip.GZipMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.security.SecurityMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "wagtail.core.middleware.SiteMiddleware",
+    "wagtail.contrib.redirects.middleware.RedirectMiddleware",
 ]
 
-ROOT_URLCONF = PROJECT_NAME + '.urls'
+ROOT_URLCONF = PROJECT_NAME + ".urls"
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = ""
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.contrib.auth.context_processors.auth',
-                'django.template.context_processors.debug',
-                'django.template.context_processors.media',
-                'django.template.context_processors.request',
-                'django.template.context_processors.static',
-                'django.contrib.messages.context_processors.messages',
-
-                'activecollab_digger.context_processors.activecollab_digger',
-
-                'wagtail.contrib.settings.context_processors.settings',
-                'wagtailmenus.context_processors.wagtailmenus',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [os.path.join(BASE_DIR, "templates")],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.contrib.auth.context_processors.auth",
+                "django.template.context_processors.debug",
+                "django.template.context_processors.media",
+                "django.template.context_processors.request",
+                "django.template.context_processors.static",
+                "django.contrib.messages.context_processors.messages",
+                "activecollab_digger.context_processors.activecollab_digger",
+                "wagtail.contrib.settings.context_processors.settings",
+                "wagtailmenus.context_processors.wagtailmenus",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = PROJECT_NAME + '.wsgi.application'
+WSGI_APPLICATION = PROJECT_NAME + ".wsgi.application"
 
 # -----------------------------------------------------------------------------
 # Authentication
 # https://docs.djangoproject.com/en/dev/ref/settings/#auth
 # -----------------------------------------------------------------------------
 
-if 'wagtail.core' in INSTALLED_APPS:
-    LOGIN_URL = '/wagtail/login/'
+if "wagtail.core" in INSTALLED_APPS:
+    LOGIN_URL = "/wagtail/login/"
 else:
-    LOGIN_URL = '/admin/login/'
+    LOGIN_URL = "/admin/login/"
 
-AUTH_LDAP_REQUIRE_GROUP = (
-    (
-        LDAPGroupQuery('cn=kdl-staff,' + LDAP_BASE_OU) | LDAPGroupQuery(
-            'cn=ego,' + LDAP_BASE_OU)
-    )
-)
+AUTH_LDAP_REQUIRE_GROUP = LDAPGroupQuery(
+    "cn=kdl-staff," + LDAP_BASE_OU
+) | LDAPGroupQuery("cn=ego," + LDAP_BASE_OU)
 
 # -----------------------------------------------------------------------------
 # Sessions
@@ -251,25 +236,25 @@ SESSION_EXPIRE_AT_BROWSER_CLOSE = True
 # https://docs.djangoproject.com/en/dev/ref/settings/#static-files
 # -----------------------------------------------------------------------------
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, STATIC_URL.strip('/'))
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, STATIC_URL.strip("/"))
 
 if not os.path.exists(STATIC_ROOT):
     os.makedirs(STATIC_ROOT)
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'assets'),
-    os.path.join(BASE_DIR, 'node_modules'),
+    os.path.join(BASE_DIR, "assets"),
+    os.path.join(BASE_DIR, "node_modules"),
 )
 
 STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-    'compressor.finders.CompressorFinder',
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+    "compressor.finders.CompressorFinder",
 )
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(STATIC_ROOT, MEDIA_URL.strip('/'))
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(STATIC_ROOT, MEDIA_URL.strip("/"))
 
 if not os.path.exists(MEDIA_ROOT):
     os.makedirs(MEDIA_ROOT)
@@ -287,7 +272,7 @@ COMPRESS_ENABLED = True
 
 COMPRESS_CSS_FILTERS = [
     # CSS minimizer
-    'compressor.filters.cssmin.CSSMinFilter'
+    "compressor.filters.cssmin.CSSMinFilter"
 ]
 
 COMPRESS_PRECOMPILERS = (
@@ -312,8 +297,8 @@ COMPRESS_PRECOMPILERS = (
     #   not dead
     # 5. inside compress js add type="text/es6" to the scripts:
     #   <script src="{% static 'js/app.js' %}" type="text/es6">
-    ('text/es6', 'npx babel {infile} -o {outfile}'),
-    ('text/x-scss', 'django_libsass.SassCompiler'),
+    ("text/es6", "npx babel {infile} -o {outfile}"),
+    ("text/x-scss", "django_libsass.SassCompiler"),
 )
 
 # -----------------------------------------------------------------------------
@@ -327,7 +312,7 @@ FABRIC_USER = getpass.getuser()
 # -----------------------------------------------------------------------------
 
 # Google Analytics ID
-GA_ID = ''
+GA_ID = ""
 
 LUNR_BUILD_INDEX = False
 
@@ -335,11 +320,11 @@ LUNR_BUILD_INDEX = False
 # ActiveCollab Digger
 # -----------------------------------------------------------------------------
 
-AC_BASE_URL = 'https://app.activecollab.com/148987'
-AC_API_URL = AC_BASE_URL + '/api/v1/'
+AC_BASE_URL = "https://app.activecollab.com/148987"
+AC_API_URL = AC_BASE_URL + "/api/v1/"
 AC_PROJECT_ID = 982
 AC_USER = 36
-AC_TOKEN = ''
+AC_TOKEN = ""
 
 # -----------------------------------------------------------------------------
 # Wagtail
@@ -351,55 +336,68 @@ WAGTAIL_PASSWORD_RESET_ENABLED = False
 WAGTAIL_SITE_NAME = PROJECT_TITLE
 
 sketchfab_provider = {
-    'endpoint': 'https://sketchfab.com/oembed',
-    'urls': [
-        r'^http(?:s)?://sketchfab\.com/models/.+$',
-    ]
+    "endpoint": "https://sketchfab.com/oembed",
+    "urls": [
+        r"^http(?:s)?://sketchfab\.com/models/.+$",
+    ],
+}
+
+youtube_provider = {
+    "endpoint": "https://www.youtube.com/oembed",
+    "urls": [
+        r"^https?://(?:[-\w]+\.)?youtube\.com/watch.+$",
+        r"^https?://(?:[-\w]+\.)?youtube\.com/v/.+$",
+        r"^https?://youtu\.be/.+$",
+        r"^https?://(?:[-\w]+\.)?youtube\.com/user/.+$",
+        r"^https?://(?:[-\w]+\.)?youtube\.com/[^#?/]+#[^#?/]+/.+$",
+        r"^https?://m\.youtube\.com/index.+$",
+        r"^https?://(?:[-\w]+\.)?youtube\.com/profile.+$",
+        r"^https?://(?:[-\w]+\.)?youtube\.com/view_play_list.+$",
+        r"^https?://(?:[-\w]+\.)?youtube\.com/playlist.+$",
+    ],
 }
 
 WAGTAILEMBEDS_FINDERS = [
     # overrides the default oEmbed provider for sketchfab
     {
-        'class': 'wagtail.embeds.finders.oembed',
-        'providers': [sketchfab_provider],
+        "class": "wagtail.embeds.finders.oembed",
+        "providers": [sketchfab_provider, youtube_provider],
     },
     # handles all other oEmbed providers the default way
     {
-        'class': 'wagtail.embeds.finders.oembed',
-    }
+        "class": "wagtail.embeds.finders.oembed",
+    },
 ]
 
 WAGTAILUSERS_PASSWORD_ENABLED = False
 WAGTAILUSERS_PASSWORD_REQUIRED = False
 
 # Wagtail Bakery: https://github.com/wagtail/wagtail-bakery
-BUILD_DIR = os.path.join(BASE_DIR, 'build')
+BUILD_DIR = os.path.join(BASE_DIR, "build")
 
-BAKERY_VIEWS = (
-    'wagtailbakery.views.AllPublishedPagesView',
-)
+BAKERY_VIEWS = ("wagtailbakery.views.AllPublishedPagesView",)
 
 # KDL Wagtail
 
 KDL_WAGTAIL_ITEMS_PER_PAGE = 50
-KDL_WAGTAIL_PERSON_MODEL = 'kdl_wagtail_people.Person'
+KDL_WAGTAIL_PERSON_MODEL = "kdl_wagtail_people.Person"
 
 KDL_WAGTAIL_KRACKDOWN_FILTERS = [
-    'kdl_wagtail.core.utils.krackdown_anchor',
-    'kdl_wagtail.core.utils.krackdown_link',
-    'kdl_wagtail.core.utils.krackdown_footnote'
+    "kdl_wagtail.core.utils.krackdown_anchor",
+    "kdl_wagtail.core.utils.krackdown_link",
+    "kdl_wagtail.core.utils.krackdown_footnote",
 ]
 
-KDL_WAGTAIL_ZOTERO_COLLECTION = ''
-KDL_WAGTAIL_ZOTERO_LIBRARY_ID = ''
-KDL_WAGTAIL_ZOTERO_LIBRARY_TYPE = ''
-KDL_WAGTAIL_ZOTERO_NOTE_STYLE = 'chicago-fullnote-bibliography'
-KDL_WAGTAIL_ZOTERO_SHORTNOTE_STYLE = 'chicago-note-bibliography'
-KDL_WAGTAIL_ZOTERO_TOKEN = ''
+KDL_WAGTAIL_ZOTERO_COLLECTION = ""
+KDL_WAGTAIL_ZOTERO_LIBRARY_ID = ""
+KDL_WAGTAIL_ZOTERO_LIBRARY_TYPE = ""
+KDL_WAGTAIL_ZOTERO_NOTE_STYLE = "chicago-fullnote-bibliography"
+KDL_WAGTAIL_ZOTERO_SHORTNOTE_STYLE = "chicago-note-bibliography"
+KDL_WAGTAIL_ZOTERO_TOKEN = ""
 
 # -----------------------------------------------------------------------------
 # Automatically generated settings
 # -----------------------------------------------------------------------------
 
 # Check which db engine to use:
-db_engine = 'django.db.backends.postgresql_psycopg2'
+db_engine = "django.db.backends.postgresql_psycopg2"
